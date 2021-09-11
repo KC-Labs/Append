c01f7680a0bc550a23d99fde04cac478fb6c5871
Patrick Cui <work@cuipatrick.com>
2021-09-11 01:16:15 -0700

    initialized basic ui

---
+++ append/helpers/extensions.swift
@@ -0,0 +1,32 @@
+//
+//  extensions.swift
+//  append
+//
+//  Created by Patrick Cui on 9/10/21.
+//
+
+import Foundation
+import UIKit
+
+public extension UIView {
+    func showAnimation(_ completionBlock: @escaping () -> Void) {
+        isUserInteractionEnabled = false
+        UIView.animate(withDuration: 0.1,
+                       delay: 0,
+                       options: .curveLinear,
+                       animations: { [weak self] in
+                        self?.transform = CGAffineTransform.init(scaleX: 0.95, y: 0.95)
+                       }) {  (done) in
+            UIView.animate(withDuration: 0.1,
+                           delay: 0,
+                           options: .curveLinear,
+                           animations: { [weak self] in
+                            self?.transform = CGAffineTransform.init(scaleX: 1, y: 1)
+                           }) { [weak self] (_) in
+                self?.isUserInteractionEnabled = true
+                completionBlock()
+            }
+                       }
+    }
+}
+
---
+++ append/helpers/global.swift
@@ -0,0 +1,24 @@
+//
+//  Colors.swift
+//  append
+//
+//  Created by Patrick Cui on 9/10/21.
+//
+
+import Foundation
+import UIKit
+
+struct Color {
+    static let bg = UIColor(red: 47 / 255, green: 52 / 255, blue: 55 / 255, alpha: 1)
+    static let card = UIColor(red: 63 / 255, green: 68 / 255, blue: 71 / 255, alpha: 1)
+    static let action = UIColor(red: 47 / 255, green: 170 / 255, blue: 220 / 255, alpha: 1)
+}
+
+struct Font {
+    struct semibold {
+        static func withSize(size: CGFloat) -> UIFont {
+            return UIFont(name: "SFProText-Semibold", size: size)!
+        }
+    }
+    
+}
