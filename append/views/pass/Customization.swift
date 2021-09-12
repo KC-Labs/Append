ace8f991e96d78f695c2ce02f14fc6c0673cf07d
Minsoo Kang <jimmykang1016@gmail.com>
2021-09-12 03:58:00 -0700

    add color support for backend

--- append/views/pass/Customization.swift
+++ append/views/pass/Customization.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import SafariServices
+import UIKit
 
 class Customization: UIViewController {
     
@@ -36,6 +38,25 @@ class Customization: UIViewController {
     }()
     private var preview = UIView()
     private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    
+    func showWalletPage() {
+        func process(walletLink: String) -> Void {
+            print("generatePass completed")
+            print("walletLink: " + walletLink)
+            
+            // open in safari
+            let config = SFSafariViewController.Configuration()
+            config.entersReaderIfAvailable = true
+
+            let safariVC = SFSafariViewController(url: URL(string: walletLink)!, configuration: config)
+            present(safariVC, animated: true)
+        }
+        
+        let indexOfColor = Color.customColors.firstIndex{$0 == pass.color}!
+        print("found color index: " + indexOfColor.description)
+        
+        PSInterface.generatePass(whenCompleted: process, mainText: pass.title, subText: pass.note!, barcodeData: pass.barcodeData!, colorIndex: indexOfColor)
+    }
     
     @objc func back() {
         if !isFinished {
